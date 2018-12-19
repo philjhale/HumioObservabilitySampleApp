d8e883876daa44e4a99d4a33b631e2162f975ee9
philjhale <philjhale@gmail.com>
2018-12-19 15:03:12 +0000

    Added extra controllers for more logging data

---
+++ WebApi/Controllers/MouseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ObservabilitySampleApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MouseController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return new OkResult();
+        }
+    }
+}
